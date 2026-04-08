5363d0dfd0fe9d908836cd10f78f45c3e20dff35
67Robin <kprobin67@gmail.com>
2026-04-08 10:38:24 +0530

    time chnage for test

--- core/views.py
+++ core/views.py
@@ -14,7 +14,7 @@ from .serializers import (
     SnackItemSerializer, OrderSerializer
 )
 
-CUTOFF_TIME = time(10, 30)
+CUTOFF_TIME = time(23, 30)
 
 def is_before_cutoff():
     return timezone.localtime().time() < CUTOFF_TIME

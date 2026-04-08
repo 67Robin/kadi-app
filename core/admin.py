be42a2e0526b31cfc65e185e34e06c0e35178905
67Robin <kprobin67@gmail.com>
2026-04-08 14:43:20 +0530

    remove duplicate SnackItem admin registration

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import User, SnackItem, Order, OrderItem
 
 admin.site.register(User)
-admin.site.register(SnackItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 @admin.register(SnackItem)
